34cb006d518340334cdeb00f634d80171355a817
NIKHIL KUMAR REDDY <nikhilkumarreddy28@gmail.com>
2022-12-11 09:56:49 +0530

    Codes as on 11 Dec

---
+++ dsa/dp/fibonacci.java
@@ -0,0 +1,15 @@
+package dsa.dp;
+
+import java.util.Scanner;
+
+public class fibonacci {
+    static int rec_fib(int n){
+        if(n<)
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the n fib series");
+        int n = s.nextInt();
+        System.out.println();
+    }
+}
